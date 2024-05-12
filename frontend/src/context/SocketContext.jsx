e4ab49cebff745ce507125ea4c5a9835052d5529
Deepak kumar <deepak7589467031@gmail.com>
2024-05-12 18:55:35 +0530

    base url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-gott.onrender.com", {
+      const socket = io("https://chat-app-yn68.onrender.com/", {
         query: {
           userId: authUser._id,
         },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 5000,
 		proxy: {
 			"/api": {
-				target: "https://chat-app-gott.onrender.com",
+				target: "https://chat-app-yn68.onrender.com",
 			},
 		},
 	},
